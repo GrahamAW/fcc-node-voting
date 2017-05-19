57ec534683f2f2e53c998ba12dd46127fadfd243
Graham Wood <graham@MacBook-Pro-2.local>
2017-05-19 17:48:27 -0500

    When a twitter user signs in, check to see if their id exists in the user database, if not add them. If they do exist, set that as the local user.

--- handlers/passport.js
+++ handlers/passport.js
@@ -10,13 +10,10 @@ passport.use(new TwitterStrategy({
     consumerSecret: process.env.CONSUMER_SECRET,
     callbackURL: 'http://127.0.0.1:7378/login/twitter/done'
   },
-  function(token, tokenSecret, profile, cb) {
-    // In this example, the user's Twitter profile is supplied as the user
-    // record.  In a production-quality application, the Twitter profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
-    return cb(null, profile);
+  async function(token, tokenSecret, profile, cb) {
+    console.log("passport twitter called!");
+    user = await User.twitterFindOrCreate(profile);
+    return cb(null, user);
   }));
 
 // TODO:
--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 const passportLocalMongoose = require('passport-local-mongoose');
-const validator = require('validator')
+const validator = require('validator');
+const chalk = require('chalk');
 
 const userSchema = mongoose.Schema({
   email: {
@@ -15,14 +17,44 @@ const userSchema = mongoose.Schema({
     type: String,
     required: 'Please supply a name.',
     trim: true
-  }
+  },
+  twitterId: String
 });
 
 userSchema.plugin(passportLocalMongoose, {
   usernameField: 'email'
 });
 
-// TODO: 
+// pre save hook to encyrpt passsword
+userSchema.pre('save', async function(next) {
+  console.log(chalk.red('TODO: encrypt password before save'));
+  next();
+});
+
+// called when a user has logged in using twitter. Finds the users profile
+// in the database, or if it doesn't exist, creates a new profile.
+userSchema.statics.twitterFindOrCreate = async function(profile) {
+  const that = this;
+  return new Promise( async function(resolve, reject) {
+    // console.log(this);
+    let user = await that.findOne({ twitterId: profile.id });
+    if(user) {
+      resolve({ user });
+    } else {
+      // setting email to profile.id@twitter because don't want to break
+      // validations for my local auth stategy.
+      await that.create({
+        email: profile.id + '@twitter.com',
+        name: profile.displayName,
+        twitterId: profile.id
+      });
+      console.log(user);
+      resolve(user);
+    }
+  });
+}
+
+// TODO:
 // userSchema.plugin(mongodbErrorHandler);
 
 
